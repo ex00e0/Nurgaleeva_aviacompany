d41291d04a2d3a339864ff93d1aa8eda25818ab7
ex00e0 <hasanova.leya@yandex.ru>
2023-12-05 11:03:48 +0500

    добавления блоков в account, orderFormPassengers

--- js/script5(orderAddServ).js
+++ js/script5(orderAddServ).js
@@ -11,27 +11,16 @@ let servWrap = document.getElementsByClassName("servWrap");
 let check0 = 0;
 let checkSelect = 0;
 let blockFood = document.getElementById("blockFood");
-function getTheStyle(element){let borderColor = window.getComputedStyle(element).borderColor;
-                               return borderColor;}
-for (let i=0;i<switchers.length;i++) {
-switchers[i].addEventListener("click", function () {if (check0 == 0) {circles[i].style.left = "60%";
-                                                                                                         circles[i].style.backgroundColor = "rgb(45, 78, 255)";
-                                                                                                         switchers[i].style.borderColor  = "rgb(45, 78, 255)";
-                                                                                                         check0++;} 
-                                                    else {
-                                                    if (check0==1) {circles[i].style.left = "5%";
-                                                                                                         circles[i].style.backgroundColor = "rgb(217, 217, 217)";
-                                                                                                         switchers[i].style.borderColor  = "rgb(217, 217, 217)";
-                                                                                                         check0--; }}});  }
 for (let j=0;j<selections.length;j++) {
 selections[j].addEventListener("click", function () {if (checkSelect == 0) {select[j].innerHTML="Скрыть";
                                                                             imgSelect[j].style.transform = "rotate(180deg)";
                                                                             servGrid[j].style.height="38vmax";
                                                                             servWrap[j].style.height="20%";
-                                                                            blockFood.style.display = "grid";
+                                                                            if (j==0) {blockFood.style.display = "grid";}
                                                                            checkSelect++;} 
+                                                            
                                                     else {if (checkSelect==1) {select[j].innerHTML="Показать";
-                                                    blockFood.style.display = "none";
+                                                    if (j==0) {blockFood.style.display = "none";}
                                                     imgSelect[j].style.transform = "rotate(360deg)";
                                                     servGrid[j].style.height="8vmax";
                                                     servWrap[j].style.height="100%";
@@ -59,8 +48,7 @@ for (let x=0; x<buttonMinus.length; x++) {buttonMinus[x].addEventListener("click
                                                                                                                                                         }  
                                                                                                                                                         if (numbersServ[0].textContent == 0 && numbersServ[1].textContent == 0 && numbersServ[2].textContent == 0) {circles[0].style.left = "5%";
                                                                                                                                                         circles[0].style.backgroundColor = "rgb(217, 217, 217)";
-                                                                                                                                                        switchers[0].style.borderColor  = "rgb(217, 217, 217)";
-                                                                                                                                                        check0--; }
+                                                                                                                                                        switchers[0].style.borderColor  = "rgb(217, 217, 217)";}
                                                                                                                                                         } } );}
 for (let x=0; x<buttonPlus.length; x++) {buttonPlus[x].addEventListener("click", function () {if (buttonPlus[x].classList.contains('activeButton')) {buttonMinus[x].classList.remove("disabledButton");
                                                                                                                                                         buttonMinus[x].classList.add("activeButton");
@@ -78,10 +66,18 @@ for (let x=0; x<buttonPlus.length; x++) {buttonPlus[x].addEventListener("click",
                                                                                                                                                                     buttonPlus[x].classList.remove("activeButton");}} 
                                                                                                                                                                     if (numbersServ[0].textContent != 0 || numbersServ[1].textContent != 0 || numbersServ[2].textContent != 0) {circles[0].style.left = "60%";
                                                                                                                                                                     circles[0].style.backgroundColor = "rgb(45, 78, 255)";
-                                                                                                                                                                    switchers[0].style.borderColor  = "rgb(45, 78, 255)";
-                                                                                                                                                                    check0++;}
+                                                                                                                                                                    switchers[0].style.borderColor  = "rgb(45, 78, 255)";}
                                                                                                                                                         } } );}
-
+let switchers2 = document.getElementsByClassName("switcher2");
+let circles2 = document.getElementsByClassName("switcherCircle2");
+for (let z=0; z<switchers2.length; z++) {switchers2[z].addEventListener("click", function () { if (check0 == 1) {circles2[z].style.left = "5%";
+                                        circles2[z].style.backgroundColor = "rgb(217, 217, 217)";
+                                        switchers2[z].style.borderColor  = "rgb(217, 217, 217)";
+                                        check0--; }
+                                           else if (check0 == 0) {circles2[z].style.left = "60%";
+                                        circles2[z].style.backgroundColor = "rgb(45, 78, 255)";
+                                        switchers2[z].style.borderColor  = "rgb(45, 78, 255)";
+                                        check0++;}} );}
 
 
 
--- js/script6(orderFormPassengers).js
+++ js/script6(orderFormPassengers).js
@@ -4,9 +4,22 @@ for (let r = 0; r < inputsFormPassenger.length; r++) {inputsFormPassenger[r].add
                                                                                                             labelsForInputsPassenger[r].style.display = "block";
                                                                                                             inputsFormPassenger[r].style.color = "black";} );        }
 
-let firstRadioButton = document.getElementById("firstRadioButton");
-let secondRadioButton = document.getElementById("secondRadioButton");
-firstRadioButton.addEventListener("click", function () {document.getElementById("circle2").style.display="none";
-                                                        document.getElementById("circle1").style.display="block";} );
-secondRadioButton.addEventListener("click", function () {document.getElementById("circle1").style.display="none";
-                                                        document.getElementById("circle2").style.display="block";} );
+let radioButtons = document.getElementsByClassName("radioButton");
+let circleRadio = document.getElementsByClassName ("circleRadio");
+for (let m=0; m<radioButtons.length; m++) {radioButtons[m].addEventListener("click", function () {if (radioButtons[m].classList.contains("firstRadioButton")) {circleRadio[m+1].style.display="none";
+                                                        circleRadio[m].style.display="block";}
+                                                        else {circleRadio[m-1].style.display="none";
+                                                        circleRadio[m].style.display="block";} } ); }
+
+
+let switchers2 = document.getElementsByClassName("switcher2");
+let circles2 = document.getElementsByClassName("switcherCircle2");
+let check0 = 0;
+for (let z=0; z<switchers2.length; z++) {switchers2[z].addEventListener("click", function () { if (check0 == 1) {circles2[z].style.left = "5%";
+                                                                                                circles2[z].style.backgroundColor = "rgb(217, 217, 217)";
+                                                                                                switchers2[z].style.borderColor  = "rgb(217, 217, 217)";
+                                                                                                check0--; }
+                                                                                                   else if (check0 == 0) {circles2[z].style.left = "65%";
+                                                                                                circles2[z].style.backgroundColor = "rgb(45, 78, 255)";
+                                                                                                switchers2[z].style.borderColor  = "rgb(45, 78, 255)";
+                                                                                                check0++;}} );}
