bfbc739d78e5597b6e5db4d35a6d9acaa193b9f9
ex00e0 <hasanova.leya@yandex.ru>
2023-12-08 13:51:13 +0300

    начало страницы администратора

--- js/script8(help).js
+++ js/script8(help).js
@@ -1,2 +1,5 @@
 let inputMessage = document.getElementById("inputMessage");
 inputMessage.addEventListener("input", function () {inputMessage.style.color="black";} );
+let inputTypeFile = document.getElementById("inputTypeFile");
+let paperclip = document.getElementById("paperclip");
+paperclip.addEventListener("click", function () {inputTypeFile.click();} );
