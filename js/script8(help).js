f4ef30f22a3da36354acfb068e156d3df003370e
ex00e0 <hasanova.leya@yandex.ru>
2023-12-08 11:47:10 +0300

    добавление поля ввода сообщения в help

--- js/script8(help).js
+++ js/script8(help).js
@@ -1,9 +1,2 @@
-let chatBlock = document.getElementById("chat");
-let addVoid1 = document.getElementById("addVoid1");
-let addVoid2 = document.getElementById("addVoid2");
-function getTheStyle(element){let height = window.getComputedStyle(element).height;
-                                return height;}
-alert (parseInt(getTheStyle(chatBlock)));
-if (parseInt(getTheStyle(chatBlock))>750) {addVoid1.style.display = "block";
-                                     addVoid2.style.display = "block";}
-else if  ((parseInt(getTheStyle(chatBlock))<820) && (parseInt(getTheStyle(chatBlock))>750)) {addVoid1.style.display = "block";}
+let inputMessage = document.getElementById("inputMessage");
+inputMessage.addEventListener("input", function () {inputMessage.style.color="black";} );
