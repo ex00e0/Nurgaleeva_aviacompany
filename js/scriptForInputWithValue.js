4ce4a5d9d311972881a2a25b0cf8349bbd2c2e8a
ex00e0 <hasanova.leya@yandex.ru>
2023-12-11 11:19:07 +0500

    поправки в adminFlightEdit

---
+++ js/scriptForInputWithValue.js
@@ -0,0 +1,5 @@
+/*let inputsA = document.getElementsByClassName("inputAdmin");
+let labelsForInputs = document.getElementsByClassName("labelAdmin");
+for (let inp = 0; inp < inputsA.length; inp++) {inputsA[inp].addEventListener("input", function () {if (inputsA[inp].value == "") {inputsA[inp].style.paddingTop = "0%";
+                                                                                                            labelsForInputs[i].style.display = "none";
+                                                                                                            inputs[i].style.color = "#6c6c6c";}} );        }*/
